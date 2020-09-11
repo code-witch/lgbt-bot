d69989f853359d37fe084f3a6ed3ce3d08fdaa9c
code-witch <code-witch@users.noreply.github.com>
2020-09-10 20:30:41 -0600

    basic commands

---
+++ commands/bad.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'bad',
+	description: 'cj cant sass me',
+	execute(message) {
+		message.channel.send('<@145358777057542144>');
+	},
+};
---
+++ commands/help.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'help',
+	description: 'help command',
+	execute(message) {
+		message.channel.send('here is the help command, uh there aint anything here... yet');
+	},
+};
